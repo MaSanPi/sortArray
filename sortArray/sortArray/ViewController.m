df8adc660b39a88b7b376dd9c06204842d612d6e
MaSanPi <1029191684@qq.com>
2018-09-13 09:40:14 +0800

    开始处理

--- sortArray/sortArray/ViewController.m
+++ sortArray/sortArray/ViewController.m
@@ -8,22 +8,35 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
+@interface ViewController ()<UITableViewDelegate,UITableViewDataSource>
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+@property(nonatomic,strong)NSArray *titleArray;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.titleArray=@[@"升序",@"降序",@"乱序",@"模型数组按时间排序",@"模型数组按时间分组排序",@"模型数组乱序"];
+    self.tableView.tableFooterView=[[UIView alloc]init];
+    
+}
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return self.titleArray.count;
+}
+
+-(UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell=[tableView dequeueReusableCellWithIdentifier:@"CELLID"];
+    if (cell==nil) {
+        cell=[[UITableViewCell alloc]initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"CELLID"];
+    }
+    
+    cell.textLabel.text=self.titleArray[indexPath.row];
+    return cell;
+    
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 
 @end
